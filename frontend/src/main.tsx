c06ff3aaa083d4fac1fe57e62fe152a14e640a84
Christy <christynguyenss@gmail.com>
2025-10-18 15:13:34 -0700

    bare bones side panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import SidePanel from './SidePanel.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SidePanel />
   </StrictMode>,
 )
